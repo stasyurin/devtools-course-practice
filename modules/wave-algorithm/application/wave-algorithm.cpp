9c850ea1d59e1672c8c79457c92a42bbc2cd9f9e
stasyurin <stanislav.unn@gmail.com>
2021-05-24 18:46:20 +0300

    style

--- modules/wave-algorithm/application/wave-algorithm.cpp
+++ modules/wave-algorithm/application/wave-algorithm.cpp
@@ -7,9 +7,9 @@
 #include "include/wave_algorithm.h"
 
 int main(int argc, const char** argv) {
-    //ComplexCalculator app;
-    //std::string output = app(argc, argv);
-    //printf("%s\n", output.c_str());
+    // ComplexCalculator app;
+    // std::string output = app(argc, argv);
+    // printf("%s\n", output.c_str());
     printf("Hello world!\n");
     return 0;
 }
---
+++ modules/wave-algorithm/src/application.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 Yurin Stanislav
+
+#include "include/application.h"
+#include "include/wave_algorithm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <string>
+#include <sstream>
